e7c49cc7c404f9b810bddf699f58d604cc93d34a
Kruaks17 <aksel.kj@hotmail.com>
2021-05-21 19:33:18 +0200

    commit

--- pages/index.js
+++ pages/index.js
@@ -1,4 +1,4 @@
-
+import Image from 'next/image';
 
 export default function Home() {
   return (
@@ -8,16 +8,32 @@ export default function Home() {
 
         <div className="logo">AK-J</div>
 
-        <ul className="menu">
+            <ul className="menu">
 
-          <h2>Works</h2>
+              <h2>Works</h2>
 
-          <h2>About</h2>
+              <h2>About</h2>
 
-          <button className="contact-btn">Contact</button>
+              <button className="contact-btn">Contact</button>
 
-        </ul>
+            </ul>
       </div>
+      <section className="ingress">
+        <h2>Designer + Front-End Developer</h2>
+        <h1 className="overskrift">Aksel<br></br>Kruse-Jensen</h1>
+      </section>
+      <section className="about-me">
+        <p>I'm Aksel, designer + front-end developer<br></br>
+        currently looking for a job.<br></br><br></br>
+        At the moment I take pride in building and developing
+        digital design, brand indetity, editorial design & packaging design.
+       <br></br><br></br> 
+        Interested in working together? <br></br><br></br>
+        Let me know!
+       </p>
+       <button className="mailbtn">Send mail</button>
+      </section>
+     
 
     </>
   )

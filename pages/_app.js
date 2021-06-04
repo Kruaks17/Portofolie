ffdf9ac24ed0126dd8762c3b156ba65cadf5b25c
Kruaks17 <aksel.kj@hotmail.com>
2021-06-04 15:15:29 +0200

    commit

--- pages/_app.js
+++ pages/_app.js
@@ -1,7 +1,14 @@
 import '../styles/globals.css'
 
 function MyApp({ Component, pageProps }) {
-  return <Component {...pageProps} />
+
+
+  return (
+    <>
+    <meta lang="en" name="Portofolie" content="Aksel viser prosjekter han har jobebt med"/>
+    <Component {...pageProps} />
+    </>
+  )
 }
 
 export default MyApp
--- pages/index.js
+++ pages/index.js
@@ -5,20 +5,13 @@ export default function Home() {
   return (
     <>
       <title>Portofolie</title>
-      <div 
-      className="header-content">
-
+      <div className="header-content">
         <div className="logo">AK-J</div>
-
-            <ul className="menu">
-
-              <h2>Works</h2>
-
-              <h2>About</h2>
-
-              <button className="contact-btn">Contact</button>
-
-            </ul>
+        <ul className="menu">
+          <h2>Works</h2>
+          <h2>About</h2>
+          <button className="contact-btn">Contact</button>
+        </ul>
       </div>
       <section className="ingress">
         <h2>Designer + Front-End Developer</h2>
@@ -29,25 +22,12 @@ export default function Home() {
         currently looking for a job.<br></br><br></br>
         At the moment I take pride in building and developing
         digital design, brand indetity, editorial design + packaging design.
-       <br></br><br></br> 
+       <br></br><br></br>
         Interested in working together? <br></br><br></br>
         Let me know!
        </p>
-       <button className="mailbtn">Send mail</button>
+        <button className="mailbtn">Send mail</button>
       </section>
-     
-
     </>
   )
 }
-
- const scrollHandler = () =>{
-
-    if (document.body.scrollTop > 20 || document.documentElement.scrollTop > 20) {
-
-         document.getElementsByClassName("header-content").style.top  = "0"
-     } else {
-       document.getElementsByClassName("header-content").style.top  = "-60"
-  
- }
- }
